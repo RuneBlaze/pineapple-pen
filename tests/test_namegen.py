36d2e1125fd9b3d8112df9264b025acbed45e933
runeblaze <runeblaze@excite.co.jp>
2024-02-17 01:45:20 -0600

    fun

---
+++ tests/test_namegen.py
@@ -0,0 +1,8 @@
+from genio.namegen import NameGenerator
+
+def test_name_generator_can_generate_name():
+    generator = NameGenerator.default()
+
+    name = generator.generate_name("m", "JP")
+    assert name.first_name
+    assert name.last_name
