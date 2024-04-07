a80f9327f2d5bc63a242c6ea7cc4fcf1902f2f69
runeblaze <runeblaze@excite.co.jp>
2024-04-07 02:26:44 -0500

    ok

--- src/genio/funccall.py
+++ src/genio/funccall.py
@@ -1,34 +1,40 @@
 import inspect
 import re
 import textwrap
-from dataclasses import dataclass, fields, is_dataclass
-from typing import Annotated, Type, Union, get_args, get_origin, Any
+from typing import Annotated, Type, Union, get_args, get_origin, Any, Literal
 
 import google.ai.generativelanguage as glm
 import google.generativeai as genai
 from icecream import ic
+from pydantic import BaseModel, ConfigDict, ValidationError
+from pydantic.fields import FieldInfo
+from pydantic.alias_generators import to_snake
+import json
 
 
-def dataclass_to_glm_schema(dc: Type) -> glm.Schema:
+def dataclass_to_glm_schema(dc: Type[BaseModel]) -> glm.Schema:
     """
-    Convert a dataclass to a Generative Language Model schema.
+    Convert a pydantic model to a Generative Language Model schema.
     """
-    if not is_dataclass(dc):
-        raise TypeError("Input must be a dataclass")
 
     properties = {}
-    for field in fields(dc):
-        field_schema = _field_type_to_glm_schema(field.type)
-        properties[field.name] = field_schema
+    for field_name, field_info in dc.model_fields.items():
+        field_schema = _field_type_to_glm_schema(field_info)
+        properties[field_name] = field_schema
 
     return glm.Schema(
         type=glm.Type.OBJECT,
         properties=properties,
-        required=[field.name for field in fields(dc) if not field.default],
+        required=[
+            field_name
+            for field_name, field_info in dc.model_fields.items()
+            if not field_info.default
+        ],
     )
 
 
-def _field_type_to_glm_schema(field_type: Type) -> glm.Schema:
+def _field_type_to_glm_schema(field_type: FieldInfo) -> glm.Schema:
+    field_type = field_type.annotation
     if get_origin(field_type) is Union:  # Handle Optional[T]
         args = get_args(field_type)
         if type(None) in args:
@@ -42,6 +48,10 @@ def _field_type_to_glm_schema(field_type: Type) -> glm.Schema:
         description = args[1]
     else:
         description = None
+
+    if get_origin(field_type) is Literal:
+        args = get_args(field_type)
+        field_type = type(args[0])
 
     # Basic type mapping
     type_map = {
@@ -60,12 +70,6 @@ def _field_type_to_glm_schema(field_type: Type) -> glm.Schema:
 
 
 def dataclass_to_function_declaration(dc: Type) -> glm.FunctionDeclaration:
-    """
-    Converts a dataclass to a GLM FunctionDeclaration.
-    """
-    if not is_dataclass(dc):
-        raise TypeError("Input must be a dataclass")
-
     name = _to_snake_case(dc.__name__)
     description = inspect.getdoc(dc)  # Get the dataclass docstring
     if description:
@@ -86,13 +90,14 @@ def _to_snake_case(name: str) -> str:
     ).lower()  # Insert '_' before Uppercase
 
 
-@dataclass
-class Attack:
+class Attack(BaseModel):
+    model_config = ConfigDict(alias_generator=to_snake)
     target: str
 
-@dataclass
-class Cast:
-    spell: str
+
+class Cast(BaseModel):
+    model_config = ConfigDict(alias_generator=to_snake)
+    spell: Literal["Hastega","Quickga","Slowga"]
     target: str
 
 
@@ -100,39 +105,64 @@ def prompt_for_structured_output(prompt: str, types: list[Type]) -> Any:
     function_decls = [dataclass_to_function_declaration(t) for t in types]
     snake2type = {_to_snake_case(t.__name__): t for t in types}
     tool = glm.Tool(function_declarations=function_decls)
-    model = genai.GenerativeModel(model_name='gemini-1.0-pro', tools=tool)
+    model = genai.GenerativeModel(model_name="gemini-1.0-pro", tools=tool)
     chat = model.start_chat()
     response = chat.send_message(prompt)
-    fc = response.candidates[0].content.parts[0].function_call
-    dictionary = type(fc).to_dict(fc)
-    name = dictionary['name']
-    try:
-        return recursive_instantiate(snake2type[name], dictionary['args'])
-    except ValueError as e:
-        chat
+    while True:
+        fc = response.candidates[0].content.parts[0].function_call
+        dictionary = type(fc).to_dict(fc)
+        name = dictionary["name"]
+        if not response.candidates[0].content.parts[0].function_call:
+            ic(response.candidates[0].content)
+            chat.send_message(f"Thanks! Given this information. Can you help me with the original request correctly?\n{prompt}")
+            continue
+        try:
+            typ = snake2type[name]
+            args = dictionary["args"]
+            return typ.model_validate(args)
+        except ValidationError as e:
+            ic(json.loads(e.json()))
+            response = chat.send_message(
+                glm.Content(
+                    parts=[
+                        glm.Part(
+                            function_response=glm.FunctionResponse(
+                                name=name,
+                                response={
+                                    "error": {
+                                        "message": "Please Retry",
+                                        "details": json.loads(e.json()),
+                                    },
+                                    "instructions": "Please Retry",
+                                },
+                            )
+                        )
+                    ]
+                )
+            )
 
 
-def recursive_instantiate(dc: Type, dictionary: dict) -> Any:
-    if not is_dataclass(dc):
-        raise TypeError("Input must be a dataclass")
+# def recursive_instantiate(dc: Type, dictionary: dict) -> Any:
+#     if not isinstance(dictionary, dict):
+#         raise TypeError("Input must be a dictionary")
 
-    if not isinstance(dictionary, dict):
-        raise TypeError("Input must be a dictionary")
+#     return dc.parse_obj(dictionary)
 
-    kwargs = {}
-    for field in fields(dc):
-        field_name = field.name
-        if field_name not in dictionary:
-            raise ValueError(f"Missing field '{field_name}' in dictionary")
+#     # kwargs = {}
+#     # for field in fields(dc):
+#     #     field_name = field.name
+#     #     if field_name not in dictionary:
+#     #         raise ValueError(f"Missing field '{field_name}' in dictionary")
 
-        field_value = dictionary[field_name]
-        if is_dataclass(field.type):
-            field_value = recursive_instantiate(field.type, field_value)
+#     #     field_value = dictionary[field_name]
+#     #     if is_dataclass(field.type):
+#     #         field_value = recursive_instantiate(field.type, field_value)
 
-        kwargs[field_name] = field_value
+#     #     kwargs[field_name] = field_value
 
-    return dc(**kwargs)
+#     # return dc(**kwargs)
 
 if __name__ == "__main__":
-    r = prompt_for_structured_output("I'm designing a turn-based combat system for a game. A goblin attacks a knight. What actions could the knight take in response? Be creative; don't just attack.", [Attack, Cast])
+    # ic(dataclass_to_glm_schema(Attack))
+    r = prompt_for_structured_output("I'm designing a turn-based combat system for a game. A goblin attacks a knight. What actions could the knight take in response? Be creative; use cast.", [Attack, Cast])
     ic(r)
